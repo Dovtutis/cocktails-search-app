b1dea3564a6851ed4f9bb3d3fd11eeab3535dcef
Dovtutis <dovtutis@gmail.com>
2021-09-14 20:15:37 +0300

    about and error components done

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 
+import "./Navbar.css";
+
 const Navbar = () => {
   return (
     <nav className='navbar'>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,19 @@
 import React from "react";
-import classes from "./About.module.css";
+
+import "./About.css";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur,
+        soluta. Quod fugiat debitis aut, aliquid voluptates ratione maxime non
+        cupiditate sit commodi est soluta eos, modi laboriosam, doloribus neque
+        beatae quo. Sunt dolores quibusdam veritatis? Laborum cum cupiditate
+        magni maxime.
+      </p>
+    </section>
   );
 };
 
