2f6c7ddc71c660f7de00bf17fd41b2afb31ac130
Dovtutis <dovtutis@gmail.com>
2021-09-14 16:17:51 +0300

    project initiated, folder and files structure in progress

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <div>
+      <h2>app component</h2>
+    </div>
+  )
+}
+
+export default App
